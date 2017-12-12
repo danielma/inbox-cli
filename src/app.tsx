b5c488e3bab7f2f34ae12b0d5091648a338830c9
Daniel Ma <drailskid@yahoo.com>
2017-12-12 10:58:02 -0800

    just tryna work

--- src/app.tsx
+++ src/app.tsx
@@ -1,100 +1,113 @@
 import React = require("react")
-import blessed = require("blessed");
-import { render } from "react-blessed/dist/fiber/fiber";
-import authorize = require("./authorize");
-import google = require("googleapis");
-import googleAuth = require("google-auth-library");
-import { exec } from "child_process";
-import { inspect } from "util";
+import blessed = require("blessed")
+import { render } from "react-blessed/dist/fiber/fiber"
+import authorize = require("./authorize")
+import google = require("googleapis")
+import googleAuth = require("google-auth-library")
+import { exec } from "child_process"
+import { inspect } from "util"
 
-const gmail = google.gmail("v1");
+interface BlessedElement {}
 
-const USE_TRELLO_DESKTOP = true;
+interface BlessedList extends BlessedElement {
+  focus(): void
+}
+
+declare namespace JSX {
+  export interface IntrinsicElements {
+    list: BlessedList
+    element: BlessedElement
+  }
+}
+
+const gmail = google.gmail("v1")
+
+const USE_TRELLO_DESKTOP = true
 
 class GmailThread {
-  _thread: any;
-  messages: [GmailMessage];
-  snippet: String;
+  _thread: any
+  messages: [GmailMessage]
+  snippet: String
 
   constructor(thread) {
-    this._thread = thread;
+    this._thread = thread
 
-    this.messages = thread.messages.map(message => new GmailMessage(message));
-    this.snippet = this._thread.snippet;
+    this.messages = thread.messages.map(message => new GmailMessage(message))
+    this.snippet = this._thread.snippet
   }
 }
 
 class GmailMessage {
-  _message: any;
-  _headers: object;
-  payload: object;
-  
+  _message: any
+  _headers: object
+  payload: object
+
   constructor(message) {
-    this._message = message;
-    this._headers = {};
+    this._message = message
+    this._headers = {}
     this._headers = message.payload.headers.reduce((memo, header) => {
-      memo[header.name.toLowerCase()] = header.value;
-      return memo;
-    }, {});
-    this.payload = message.payload;
+      memo[header.name.toLowerCase()] = header.value
+      return memo
+    }, {})
+    this.payload = message.payload
   }
 
   get id() {
-    return this._message.id;
+    return this._message.id
   }
 
   get threadId() {
-    return this._message.threadId;
+    return this._message.threadId
   }
 
   get subject() {
-    return this._headers["subject"];
+    return this._headers["subject"]
   }
 
   get plainText() {
-    const { parts } = this._message.payload;
+    const { parts } = this._message.payload
 
-    if (!parts) return "";
+    if (!parts) return ""
 
-    const plainText = parts.find(p => p.mimeType === "text/plain") || parts[0];
+    const plainText = parts.find(p => p.mimeType === "text/plain") || parts[0]
 
-    if (!plainText.body.data) return "";
+    if (!plainText.body.data) return ""
 
-    return new Buffer(plainText.body.data, "base64").toString("utf8");
+    return new Buffer(plainText.body.data, "base64").toString("utf8")
   }
 
   get externalURL() {
-    return this.githubURL || this.trelloURL;
+    return this.githubURL || this.trelloURL
   }
 
   get githubURL() {
-    const match = this.plainText.match(/github:\s+(http.+)/im);
+    const match = this.plainText.match(/github:\s+(http.+)/im)
 
-    return match && match[1];
+    return match && match[1]
   }
 
   get trelloURL() {
-    const match = this.plainText.match(/\((https:\/\/trello.com.+?)\)/);
+    const match = this.plainText.match(/\((https:\/\/trello.com.+?)\)/)
 
-    if (!match) return null;
+    if (!match) return null
 
-    const originalURL = match[1];
+    const originalURL = match[1]
 
     if (USE_TRELLO_DESKTOP) {
-      return originalURL.replace("https://", "trello://");
+      return originalURL.replace("https://", "trello://")
     } else {
-      return originalURL;
+      return originalURL
     }
   }
 
   open({ background = false } = {}) {
     return new Promise((resolve, reject) => {
       if (this.externalURL) {
-        resolve(openURL(this.externalURL, { background }));
+        resolve(openURL(this.externalURL, { background }))
       } else {
-        reject("no external URL");
+        reject("no external URL")
       }
-    });
+    })
   }
 }
 
@@ -103,45 +116,47 @@ function promisify(fn) {
     return new Promise((resolve, reject) => {
       fn(...args, (err, response) => {
         if (err) {
-          reject(err);
+          reject(err)
         }
-        resolve(response);
-      });
-    });
-  };
+        resolve(response)
+      })
+    })
+  }
 }
 
-const listThreads = promisify(gmail.users.threads.list);
-const getThread = promisify(gmail.users.threads.get);
-const modifyThread = promisify(gmail.users.threads.modify);
+const listThreads = promisify(gmail.users.threads.list)
+const getThread = promisify(gmail.users.threads.get)
+const modifyThread = promisify(gmail.users.threads.modify)
 
 function openURL(url, { background = false } = {}) {
-  const backgroundOption = background ? "-g" : "";
-  exec(`open ${backgroundOption} ${url}`);
+  const backgroundOption = background ? "-g" : ""
+  return exec(`open ${backgroundOption} ${url}`)
 }
 
 class App extends React.Component {
+  private messageList: BlessedList
+
   state = {
     threads: [],
     selectedIndex: null,
     error: null,
     lastArchivedThreadId: null
-  };
+  }
 
   componentDidMount() {
-    this.reloadInbox();
-    this.refs.messageList.focus();
-    this.setupReloadInterval();
+    this.reloadInbox()
+    this.messageList.focus()
+    this.setupReloadInterval()
   }
 
   setupReloadInterval = () => {
-    setInterval(this.reloadInbox, 30000);
-  };
+    setInterval(this.reloadInbox, 30000)
+  }
 
   reloadInbox = () => {
-    this.setState({ error: "loading" });
-    const { auth } = this.props;
-    const userId = "me";
+    this.setState({ error: "loading" })
+    const { auth } = this.props
+    const userId = "me"
     return listThreads({
       auth,
       userId,
@@ -150,35 +165,35 @@ class App extends React.Component {
       .then(({ threads }) => {
         return Promise.all(
           (threads || []).map(thread => {
-            return getThread({ auth, userId, id: thread.id });
+            return getThread({ auth, userId, id: thread.id })
           })
-        );
+        )
       })
       .then(threads => threads.map(t => new GmailThread(t)))
-      .then(threads => this.setState({ threads, error: null }));
-  };
+      .then(threads => this.setState({ threads, error: null }))
+  }
 
   handleMessageListKeypress = (_ch, key) => {
-    const { full } = key;
-    const { messageList } = this.refs;
-    const { messages } = this;
-    const selectedMessage = messages[messageList.selected];
+    const { full } = key
+    const { messageList } = this.refs
+    const { messages } = this
+    const selectedMessage = messages[messageList.selected]
     if (full === "C-o") {
-      selectedMessage.open({ background: true }).catch(this.logError);
+      selectedMessage.open({ background: true }).catch(this.logError)
     } else if (full === "C-d") {
-      this.archiveThread(selectedMessage.threadId);
+      this.archiveThread(selectedMessage.threadId)
     } else if (full === "C-p") {
-      messageList.up();
-      messageList.screen.render();
+      messageList.up()
+      messageList.screen.render()
     } else if (full === "C-n") {
-      messageList.down();
-      messageList.screen.render();
+      messageList.down()
+      messageList.screen.render()
     } else if (full === "C-z" && this.state.lastArchivedThreadId) {
-      this.unarchiveLastArchivedThread();
+      this.unarchiveLastArchivedThread()
     } else if (full === "r") {
-      this.reloadInbox();
+      this.reloadInbox()
     }
-  };
+  }
 
   archiveThread = threadId => {
     modifyThread({
@@ -187,10 +202,10 @@ class App extends React.Component {
       id: threadId,
       resource: { removeLabelIds: ["INBOX"] }
     }).then(thread => {
-      this.setState({ lastArchivedThreadId: thread.id });
-      this.reloadInbox();
-    }, this.logError);
-  };
+      this.setState({ lastArchivedThreadId: thread.id })
+      this.reloadInbox()
+    }, this.logError)
+  }
 
   unarchiveLastArchivedThread = () => {
     modifyThread({
@@ -199,36 +214,36 @@ class App extends React.Component {
       id: this.state.lastArchivedThreadId,
       resource: { addLabelIds: ["INBOX"] }
     }).then(_thread => {
-      this.setState({ lastArchivedThreadId: null });
-      this.reloadInbox();
-    }, this.logError);
-  };
+      this.setState({ lastArchivedThreadId: null })
+      this.reloadInbox()
+    }, this.logError)
+  }
 
   logError = error => {
-    this.setState({ error });
-  };
+    this.setState({ error })
+  }
 
   get messages() {
     return this.state.threads.reduce((memo, thread) => {
-      return memo.concat(thread.messages);
-    }, []);
+      return memo.concat(thread.messages)
+    }, [])
   }
 
   get messageSubjects() {
     return this.state.threads.reduce((memo, thread) => {
-      const firstSubject = thread.messages[0].subject;
+      const firstSubject = thread.messages[0].subject
       const restSubjects = thread.messages
         .slice(1)
-        .map((m, index) => `  ${m.subject}${"\0".repeat(index)}`);
+        .map((m, index) => `  ${m.subject}${"\0".repeat(index)}`)
 
-      return memo.concat([firstSubject, ...restSubjects]);
-    }, []);
+      return memo.concat([firstSubject, ...restSubjects])
+    }, [])
   }
 
   render() {
-    const { error } = this.state;
-    const { messages, messageSubjects } = this;
-    const selectedMessage = messages[this.state.selectedIndex || 0];
+    const { error } = this.state
+    const { messages, messageSubjects } = this
+    const selectedMessage = messages[this.state.selectedIndex || 0]
 
     return (
       <element>
@@ -241,13 +256,13 @@ class App extends React.Component {
           vi
           keys
           onSelectItem={(_item, index) => {
-            this.setState({ selectedIndex: index });
+            this.setState({ selectedIndex: index })
           }}
           onSelect={(_item, index) => {
-            messages[index].open().catch(this.logError);
+            messages[index].open().catch(this.logError)
           }}
           onKeypress={this.handleMessageListKeypress}
-          ref="messageList"
+          ref={ref => (this.messageList = ref)}
         />
         <box
           border={{ type: "line" }}
@@ -272,7 +287,7 @@ class App extends React.Component {
           </box>
         )}
       </element>
-    );
+    )
   }
 }
 
@@ -282,12 +297,12 @@ authorize().then(auth => {
     autoPadding: true,
     smartCSR: true,
     title: "inbox"
-  });
+  })
 
   // Adding a way to quit the program
   screen.key(["q", "C-c"], function(ch, key) {
-    return process.exit(0);
-  });
+    return process.exit(0)
+  })
 
-  const component = render(<App auth={auth} />, screen);
-});
+  const component = render(<App auth={auth} />, screen)
+})
