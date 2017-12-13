5479fe7b9eada0c4def1ce7dede44f7f5cff0ec7
Daniel Ma <drailskid@yahoo.com>
2017-12-12 16:18:11 -0800

    clean up unused code

--- src/authorize.js
+++ src/authorize.js
@@ -111,34 +111,3 @@ function storeToken(token) {
   fs.writeFile(TOKEN_PATH, JSON.stringify(token))
   console.log("Token stored to " + TOKEN_PATH)
 }
-
-/**
- * Lists the labels in the user's account.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-function listLabels(auth) {
-  var gmail = google.gmail("v1")
-  gmail.users.labels.list(
-    {
-      auth: auth,
-      userId: "me"
-    },
-    function(err, response) {
-      if (err) {
-        console.log("The API returned an error: " + err)
-        return
-      }
-      var labels = response.labels
-      if (labels.length == 0) {
-        console.log("No labels found.")
-      } else {
-        console.log("Labels:")
-        for (var i = 0; i < labels.length; i++) {
-          var label = labels[i]
-          console.log("- %s", label.name)
-        }
-      }
-    }
-  )
-}
