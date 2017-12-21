d6d2fd6c1a2e4932bb36c9176066698fb2d982e5
Daniel Ma <drailskid@yahoo.com>
2017-12-21 11:14:29 -0800

    update release script

--- release.js
+++ release.js
@@ -1,17 +1,53 @@
 var exec = require("child_process").exec
 var chalk = require("chalk")
 var npmInfo = require("./package")
+var readline = require("readline")
+var fs = require("fs")
 
 // main
 
-const version = `v${npmInfo.version}`
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
-execute("npm publish")
-  .then(() => execute(`git tag -a ${version} -m "release ${version}"`))
-  .then(() => execute(`git push origin ${version}`))
-  .catch(err => console.log(err))
+console.log(`Current version: ${npmInfo.version}`)
+
+let version = ""
+
+ask("New version: ")
+  .then(responseVersion => {
+    version = responseVersion.trim()
+
+    return ask(`Confirm: ${version}`)
+  })
+  .then(() => {
+    const newPackage = { ...npmInfo, version }
+
+    fs.writeFileSync("./package.json", JSON.stringify(newPackage, null, 2), { encoding: "utf8" })
+
+    return execute("git add package.json")
+  })
+  .then(assertGitClean)
+  .then(() => execute(`git commit -m v${version}`))
+  .then(() => execute(`git push origin master`))
+  .then(() => execute(`git tag -a v${version} -m "release v${version}"`))
+  .then(() => execute(`git push origin v${version}`))
+  .catch(err => {
+    console.log(err)
+
+    process.exit(1)
+  })
 
 // utils
+
+function ask(question) {
+  return new Promise((resolve, reject) => {
+    rl.question(question, function(response) {
+      resolve(response)
+    })
+  })
+}
 
 function execute(command) {
   console.log(chalk.magenta("execute: ") + `"${command}"`)
@@ -26,5 +62,13 @@ function execute(command) {
       console.log(chalk.green("ok! 👍"))
       resolve(stdout)
     }).stdout.pipe(process.stdout)
+  })
+}
+
+function assertGitClean() {
+  return execute("git diff --shortstat 2> /dev/null | tail -n1").then(out => {
+    if (out.trim() !== "") {
+      throw "Git isn't clean"
+    }
   })
 }
